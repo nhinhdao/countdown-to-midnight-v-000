80d6b4b240b1516e3b3a1dca1de6d13f046f9d2f
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-17 20:10:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(time)
+      loop do
+            puts "#{time} SECOND(S)!"
+            time -= 1
+            break if time == 0
+      end
+      "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(second)
+      sleep second
+      # break
+end
+
+countdown_with_sleep(6)
