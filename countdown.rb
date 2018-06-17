7657a2cf9780fd550fe841de0c81839dca7e3d82
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-17 19:58:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(time)
+      loop do
+            prints "#{time} SECONDS"
+            time -= 1
+            break if time == 1
+      end
+      
+      "HAPPY NEW YEAR!"
 end
